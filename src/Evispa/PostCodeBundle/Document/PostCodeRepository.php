4c70a14b5feaa8f8ac0c581fe413d16a70ff6bcd
Darius <darius@darius.(none)>
2013-08-23 21:35:39 +0300

    twitter bootstrap && municipalities

---
+++ src/Evispa/PostCodeBundle/Document/PostCodeRepository.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * User: darius
+ * Date: 8/23/13
+ * Time: 7:47 PM
+ */
+
+namespace Evispa\PostCodeBundle\Document;
+
+
+class PostCodeRepository
+{
+
+}
---
+++ src/Evispa/PostCodeBundle/Resource/Municipality.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * User: darius
+ * Date: 8/23/13
+ * Time: 7:58 PM
+ */
+
+namespace Evispa\PostCodeBundle\Resource;
+
+
+class Municipality
+{
+
+}
